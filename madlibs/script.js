c991e0562dfbc0663c2234b061a60e9a0b6e7db7
katelyngun <khgunawan@ucdavis.edu>
2026-05-10 20:53:47 -0700

    Update script.js

--- madlibs/script.js
+++ madlibs/script.js
@@ -4,70 +4,74 @@
   const form = document.querySelector('#madlibs');
   const result = document.querySelector('#result');
 
-  form.addEventListener('submit', function(e){
-    e.preventDefault();
+form.addEventListener('submit', function(e){
+  e.preventDefault();
 
-    const noun1 = document.querySelector('#noun1').value;
-    const adj1 = document.querySelector('#adj1').value;
-    const noun2 = document.querySelector('#noun2').value;
-    const creature1 = document.querySelector('#creature1').value;
+  const noun1 = document.querySelector('#noun1').value;
+  const adj1 = document.querySelector('#adj1').value;
+  const noun2 = document.querySelector('#noun2').value;
+  const creature1 = document.querySelector('#creature1').value;
 
-    const creature2 = document.querySelector('#creature2').value;
-    const noun3 = document.querySelector('#noun3').value;
+  const creature2 = document.querySelector('#creature2').value;
+  const noun3 = document.querySelector('#noun3').value;
 
-    const adverb = document.querySelector('#adverb').value;
-    const weapon = document.querySelector('#weapon').value;
+  const adverb = document.querySelector('#adverb').value;
+  const weapon = document.querySelector('#weapon').value;
 
-    const verb1 = document.querySelector('#verb1').value;
-    const food1 = document.querySelector('#food1').value;
+  const verb1 = document.querySelector('#verb1').value;
+  const food1 = document.querySelector('#food1').value;
 
-    const food2 = document.querySelector('#food2').value;
-    const singer = document.querySelector('#singer').value;
+  const food2 = document.querySelector('#food2').value;
+  const singer = document.querySelector('#singer').value;
 
-const story = `
+  const story = `
+  <p>
+  Once upon a time…<br><br>
 
+  There was a magical princess, the Princess Kate of the <b>${noun1}</b> Kingdom. 
+  She was a <b>${adj1}</b> princess, with the magical ability to blast 
+  <b>${noun2}</b> from her magic wand. 
+  She never goes on adventures without her pet <b>${creature1}</b>, 
+  who’s basically her sidekick!
+  <br><br>
 
+  One day, Princess Kate’s kingdom was under attack by the super bad and scary villain: 
+  Mister <b>${creature2}</b> of the <b>${noun3}</b> Planet!!! OH NO!!!!
+  <br><br>
 
-<p>
-Once upon a time…<br><br>
+  Kingdom citizens were <b>${adverb}</b> in terror as the villain cast a big, scary spell using his 
+  <b>${weapon}</b> of destruction. Things were looking grim…
+  <br><br>
 
-There was a magical princess, the Princess Kate of the <b>${noun1}</b> Kingdom. 
-She was a <b>${adj1}</b> princess, with the magical ability to blast 
-<b>${noun2}</b> from her magic wand. 
-She never goes on adventures without her pet <b>${creature1}</b>, 
-who’s basically her sidekick!
-<br><br>
+  BUT DON’T FRET! Princess Kate is here to save the day!
+  <br><br>
 
-One day, Princess Kate’s kingdom was under attack by the super bad and scary villain: 
-Mister <b>${creature2}</b> of the <b>${noun3}</b> Planet!!! OH NO!!!!
-<br><br>
+  She pointed her wand at the sky and screamed out her famous magical catchphrase: 
+  “<b>${verb1}</b> <b>${food1}</b>!!!!” 
+  An enormous sparkly rainbow blasted out her wand and pierced through the darkness, 
+  ridding the kingdom of the evil spell.
+  <br><br>
 
-Kingdom citizens were <b>${adverb}</b> in terror as the villain cast a big, scary spell using his 
-<b>${weapon}</b> of destruction. Things were looking grim…
-<br><br>
+  She offered her hand to the villain, who was wallowing in his defeat, and said: 
+  “You don’t have to be the bad guy!” The villain smiled and they became best friends.
+  <br><br>
 
-BUT DON’T FRET! Princess Kate is here to save the day!
-<br><br>
+  And just like that, the kingdom was saved. Everyone threw a big party with their favorite food, 
+  <b>${food2}</b>, and danced the night away to <b>${singer}</b>.
+  </p>
 
-She pointed her wand at the sky and screamed out her famous magical catchphrase: 
-“<b>${verb1}</b> <b>${food1}</b>!!!!” 
-An enormous sparkly rainbow blasted out her wand and pierced through the darkness, 
-ridding the kingdom of the evil spell.
-<br><br>
+  <button id="restart">Start Over</button>
+  `;
 
-She offered her hand to the villain, who was wallowing in his defeat, and said: 
-“You don’t have to be the bad guy!” The villain smiled and they became best friends.
-<br><br>
+  result.innerHTML = story;
+  form.style.display = "none";
+  form.reset();
 
-And just like that, the kingdom was saved. Everyone threw a big party with their favorite food, 
-<b>${food2}</b>, and danced the night away to <b>${singer}</b>.
-</p>
-`;
-
-    result.innerHTML = story;
-    form.style.display = "none";
-
-    form.reset();
+  // restart button functionality
+  const restartBtn = document.querySelector('#restart');
+  restartBtn.addEventListener('click', function(){
+    result.innerHTML = "";
+    form.style.display = "flex";
   });
 
-})();
+});
